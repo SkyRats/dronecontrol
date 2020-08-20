988e2db7419a7bb9602cd249e3304b74bfada389
namitopier <pierluigi0609@gmail.com>
2020-08-20 20:03:26 -0300

    Update Cylinder_Det.py
    Agora ignora os zeros!

--- scripts/Cylinder_Det.py
+++ scripts/Cylinder_Det.py
@@ -4,7 +4,7 @@ res = 60   # Resolução do lidar
 r = 25     #[cm] - Raio dos cilindros (Especificado no edital)
 d_min = 10 #[cm] - Distancia minima permitida pro Drone chegar perto de algo
 
-sensor = np.array([0,0,0,0,10,12,10,9,6,15,8,10,0,0,30,30,30,30,30,30,30,30,30,30,30,30,30,0,0,12,14,17,40,45,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) # Dado do lidar com tamanho "res"
+sensor = np.array([0,0,0,0,10,12,10,9,6,15,8,10,0,0,30,30,34,37,32,36,38,30,30,30,30,30,30,0,0,12,14,17,40,45,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) # Dado do lidar com tamanho "res"
 
 def prop_max ():
     return (1/np.pi)*np.arccos((r+d_min)/(np.sqrt(2*r**2+2*r*d_min+d_min**2)))
@@ -22,45 +22,87 @@ def detectar ():
 
     objetos = []                    # A lista de objetos que vão ser devolvidas
     #sensor_ext = sensor.append(sensor[0:int(prop_max*res)])    # Aqui eu tava tentando extender a lista, mas é mais complicado que fazer isso
-    dists = sensor[sensor>0]        # Lista ignorando os zeros
-    dists = np.append(dists, -1000) # Indica o final dessa lista pro código funfar certinho
+    #dists = sensor[sensor>0]        # Lista ignorando os zeros
+    #dists = np.append(dists, -1000) # Indica o final dessa lista pro código funfar certinho
+    sensor_ext = np.append(sensor, -1000)
     temp = []                       # Lista temporaria pra guardar os objetos que vão ser inseridos em "objetos"
     indices = []                    # Lista dos indices da lista "dists" que representam os cilindros
     indices_temp = []               # Armazena os indices que vao ser colocados em "indices"
-    elemento = dists[0]             # Inicia a analise com o primeiro elemento
-    indice = 0
+    elemento = 0             # Inicia a analise com o primeiro elemento
+    indice_ini = 0
 
+    for i in range (len(sensor)):
+        if (sensor[i] != 0):
+            indice_ini = i
+            elemento = sensor[i]
+            print(elemento)
+            break
+    indice = indice_ini
     temp.append(elemento)
     indices_temp.append(indice)
 
-    for i in range (1, len (dists)):
+    
+    for i in range (indice_ini+1, len (sensor_ext)):
         
-        if (abs(dists[i] - elemento) <= varMax): # Vê se o próximo elemento da lista tem uma variação "permitida" de acordo com a fórmula
-            temp.append(dists[i])
+        if (sensor_ext[i] != 0):
+            
+            elemento = sensor_ext[i-1]
+            indice = i-1
 
-        elif ((len(temp)/res) > prop_max):      # Se o proximo elemento não for mais um objeto de acordo com varMax, o codigo analisa o tamanho do objeto e faz a proporção pra ver se pode ser um cilindro (se nao, é chao)
-            temp = []                           # Nesse caso, seria chão, então reinicializa as listas temporarias
-            indices_temp = []
-            temp.append(dists[i])
-            indices_temp.append(i)
+            if (abs(sensor_ext[i] - elemento) <= varMax): # Vê se o próximo elemento da lista tem uma variação "permitida" de acordo com a fórmula
+                temp.append(sensor_ext[i])
 
-        else:                                   # Se nenhum dos de cima, então é um cilindro. Ai o codigo bota nas listas "objetos" e "indices" e dps reinicializa os temporarios
-            objetos.append(temp)
-            indices_temp.append(indice)
-            indices.append(indices_temp)
+            elif ((len(temp)/res) > prop_max):      # Se o proximo elemento não for mais um objeto de acordo com varMax, o codigo analisa o tamanho do objeto e faz a proporção pra ver se pode ser um cilindro (se nao, é chao)
+                temp = []                           # Nesse caso, seria chão, então reinicializa as listas temporarias
+                indices_temp = []
+                temp.append(sensor_ext[i])
+                indices_temp.append(i)
 
-            temp = []
-            indices_temp = []
-            temp.append(dists[i])
-            indices_temp.append(i)
+            else:                                   # Se nenhum dos de cima, então é um cilindro. Ai o codigo bota nas listas "objetos" e "indices" e dps reinicializa os temporarios
+                objetos.append(temp)
+                indices_temp.append(indice)
+                indices.append(indices_temp)
+
+                temp = []
+                indices_temp = []
+                temp.append(sensor_ext[i])
+                indices_temp.append(i)
+                
+
+            
+    
+    
+    
+    # for i in range (1, len (dists)):
+        
+    #     if (abs(dists[i] - elemento) <= varMax): # Vê se o próximo elemento da lista tem uma variação "permitida" de acordo com a fórmula
+    #         temp.append(dists[i])
+
+    #     elif ((len(temp)/res) > prop_max):      # Se o proximo elemento não for mais um objeto de acordo com varMax, o codigo analisa o tamanho do objeto e faz a proporção pra ver se pode ser um cilindro (se nao, é chao)
+    #         temp = []                           # Nesse caso, seria chão, então reinicializa as listas temporarias
+    #         indices_temp = []
+    #         temp.append(dists[i])
+    #         indices_temp.append(i)
+
+    #     else:                                   # Se nenhum dos de cima, então é um cilindro. Ai o codigo bota nas listas "objetos" e "indices" e dps reinicializa os temporarios
+    #         objetos.append(temp)
+    #         indices_temp.append(indice)
+    #         indices.append(indices_temp)
+
+    #         temp = []
+    #         indices_temp = []
+    #         temp.append(dists[i])
+    #         indices_temp.append(i)
             
 
-        elemento = dists[i]
-        indice = i
+    #     elemento = dists[i]
+    #     indice = i
 
     return objetos, indices
+
 
 objetos, indices = detectar()
 
 print("As distancias dos objetos detectados são: {}".format(objetos))
 print("Os intervalos dos indices dos objetos detectados são: {}".format(indices))
+print(prop_max*res)
